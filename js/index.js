acba9e0a2b03f9eca381442700b95fc8e1ba74ab
ian tan <ian_tan@hotmail.com>
2017-07-15 20:30:49 +0800

    first

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "80px";
+
+/*$(document).ready(function(){
+  $(".col-md-3").mouseenter(function(){
+    $(this).fadeTo("fast", 0.5)
+  });
+  $(".col-md-3").mouseleave(function(){
+    $(this).fadeTo("fast", 1)
+  })
+}) */
